4b85e392672711f347e3157d6a1bd15d48e99957
chisha <591928179@qq.com>
2017-10-15 01:58:21 +0800

    ci

--- app/src/main/java/com/diaozhatian/zhazhanote/http/Api.java
+++ app/src/main/java/com/diaozhatian/zhazhanote/http/Api.java
@@ -1,8 +1,14 @@
 package com.diaozhatian.zhazhanote.http;
 
+import com.audienl.superlibrary.utils.encrypt.Base64Utils;
+import com.audienl.superlibrary.utils.encrypt.RSAUtils;
 import com.diaozhatian.zhazhanote.Constants;
 
 import org.xutils.http.RequestParams;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 
 import io.reactivex.Observable;
 
@@ -13,16 +19,25 @@ import io.reactivex.Observable;
  */
 public class Api {
     /** 注册 */
-//    public static Observable<HttpResult> postRegister(String mobile, String password) {
-//        final RequestParams params = new RequestParams(Constants.URL_AUTH_REGISTER);
-//        params.addBodyParameter("mobile", mobile);
-//        PublicKey key = new
-//        params.addBodyParameter("password", password);
-//        params.addBodyParameter("sex", sex);
-//        params.addBodyParameter("smsCode", smsCode);
-//        params.addBodyParameter("clientType", clientType);
-//        return Observable.create(e -> new Http<>(HttpResult.class, e, "注册").get(params));
-//    }
+    public static Observable<HttpResult> postRegister(String mobile, String password) {
+        final RequestParams params = new RequestParams(Constants.URL_AUTH_REGISTER);
+        params.addBodyParameter("mobile", mobile);
+        PublicKey key = new params.addBodyParameter("password", password);
+        params.addBodyParameter("sex", sex);
+        params.addBodyParameter("smsCode", smsCode);
+        params.addBodyParameter("clientType", clientType);
+        return Observable.create(e -> {
+            // 加密密码
+            password = Base64Utils.encode(password.getBytes());
+            try {
+                PublicKey publicKey = RSAUtils.getPublicKey(Constants.RSA_MODULUS, Constants.RSA_PUBLIC_EXPONENT);
+                password = String.valueOf(RSAUtils.encryptData(password.getBytes(), publicKey));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            new Http<>(HttpResult.class, e, "注册").get(params);
+        });
+    }
 
     /** 获取验证码 */
     public static Observable<HttpResult> getCode(String mobile) {
---
+++ app/src/main/java/com/diaozhatian/zhazhanote/http/ApiEncryptUtils.java
@@ -0,0 +1,8 @@
+package com.diaozhatian.zhazhanote.http;
+
+/**
+ * 描述：
+ * <p>
+ * Created by audienl@qq.com on 2017/10/15.
+ */
+public class ApiEncryptUtils {}
